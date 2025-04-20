d0cd52e011b417e2e94de535645a7cff734bfbe8
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-20 15:02:59 +0200

    One more rename

--- project/settings.py
+++ project/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'app' / 'templates'],
+        'DIRS': [BASE_DIR / 'import_receipt' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
