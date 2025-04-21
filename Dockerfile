85c65cedab40f846c1523d881f76e6f813644ff5
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-21 15:13:06 +0200

    add models

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /code
 COPY . /code
 
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip install poetry
 
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- import_receipt/models.py
+++ import_receipt/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=255)
+    rec_id = models.CharField(max_length=64)
+    location = models.CharField(max_length=255)
+
+
+class Currency(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=5)
+
+
+class Receipt(models.Model):
+    def __str__(self):
+        return self.number
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL)
+    number = models.CharField(max_length=64)
+    date_time = models.DateTimeField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    def get_default_category(self):
+        category, _ = self.objects.get_or_create(name="Others")
+        return category.id
+    name = models.CharField(max_length=127, unique=True)
+
+
+class Items(models.Model):
+    def __str__(self):
+        return self.description
+    description = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    tax_base = models.DecimalField(max_digits=9, decimal_places=2)
+    tax_rate = models.DecimalField(max_digits=4, decimal_places=2)
+    tax = models.DecimalField(max_digits=9, decimal_places=2)
+    total = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=Category.get_default_category,
+                                 null=False, blank=False)
+    receipt = models.ForeignKey(Receipt, on_delete=models.SET_NULL)
